67a47293c50976ce37adbaa49168d282663b5b2d
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-27 08:59:28 +0600

    random user info

---
+++ js/randomBuddy.js
@@ -0,0 +1,17 @@
+const randomBuddy = () => {
+ fetch('https://randomuser.me/api/?results=5')
+ .then(response => response.json())
+ .then(data => getdata(data));
+}
+randomBuddy();
+const getdata =(buddyData)=>{
+    console.log(buddyData);
+    const buddyAll = buddyData.results;
+    const dataBlock = document.getElementById('random-buddy');
+    for(const buddy of buddyAll){
+        const p = document.createElement('p');
+        p.innerText = `name: ${buddy.name.title} ${buddy.name.first} ${buddy.name.last} email: ${buddy.email} location : ${buddy.location.country}`;
+        dataBlock.appendChild(p);
+    }
+    
+}
